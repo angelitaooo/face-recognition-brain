07b4f1962e892b0c6c64297b7d6d4760cb6aaae1
Angela Ordonez <angelitaooo@me.com>
2018-11-12 15:58:29 -0500

    conect sign in with the backend

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,6 +1,41 @@
 import React, { Component } from 'react';
 
 class Signin extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      signInEmail: '',
+      signInPassword: ''
+    }
+  }
+
+  onEmailChange = (event) => {
+    this.setState({signInEmail: event.target.value});
+  }
+
+  onPasswordChange = (event) => {
+    this.setState({signInPassword: event.target.value});
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:3001/signin', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        email: this.state.signInEmail,
+        password: this.state.signInPassword
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      if(data === 'success') {
+        this.props.onRouteChange('home');
+      }
+    })
+  }
+
   render() {
     const { onRouteChange } = this.props;
     return (
@@ -18,6 +53,7 @@ class Signin extends Component {
                   type="email"
                   name="email-address"
                   id="email-address"
+                  onChange={this.onEmailChange}
                 />
               </div>
               <div className="mv3">
@@ -29,14 +65,13 @@ class Signin extends Component {
                   type="password"
                   name="password"
                   id="password"
+                  onChange={this.onPasswordChange}
                 />
               </div>
             </fieldset>
             <div className="">
               <input
-                onClick={() => {
-                  onRouteChange('home');
-                }}
+                onClick={ this.onSubmit }
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                 type="submit"
                 value="Sign in"
